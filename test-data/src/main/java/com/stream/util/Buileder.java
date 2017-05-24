94cf1ddbf4c7461eb1c33590d41f11ab0f71dc01
huangkaitao <zijia@yiji.com>
2017-05-24 17:38:54 +0800

    请求包构建

---
+++ test-data/src/main/java/com/stream/util/Buileder.java
@@ -0,0 +1,27 @@
+package com.stream.util;
+
+import com.stream.order.Event;
+import com.yjf.common.id.GID;
+import com.yjf.common.id.OID;
+import com.yjf.common.util.RandomStringUtils;
+
+/**
+ * Created by alpha on 2017/5/24.
+ */
+public class Buileder {
+    public Event buildEvent(){
+        String bankCardAccount = Constant.BANKCARD_PRIFIX + RandomStringUtils.random(5, RandomStringUtils.NUMERIC_CHARS_STR);
+        String userId = Constant.USERID_PRIFIX + RandomStringUtils.random(5, RandomStringUtils.NUMERIC_CHARS_STR);
+        String partyUserId = Constant.USERID_PRIFIX + RandomStringUtils.random(5, RandomStringUtils.NUMERIC_CHARS_STR);
+
+        Event event = new Event();
+        event.setGid(GID.newGID());
+        event.setBizNo(OID.newID());
+        event.setOutBizNo(OID.newID());
+        event.setBankAccountNo(bankCardAccount);
+        event.setCounterPartyUserId(partyUserId);
+        event.setUserId(userId);
+
+        return event;
+    }
+}
