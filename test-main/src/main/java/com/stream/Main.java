6d12b129efd1dee4afc8fc702b13561c10331a9a
zhangkirk@sohu.com <zhangkirk>
2017-05-24 14:27:58 +0800

    1111111111111

---
+++ test-main/src/main/java/com/stream/Main.java
@@ -0,0 +1,14 @@
+package com.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by alpha on 2017/5/24.
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
