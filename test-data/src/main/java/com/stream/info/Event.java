7f56c60243e9a214519c26abcdbc7ee5fa527d2a
huangkaitao <zijia@yiji.com>
2017-05-25 14:49:48 +0800

    请求包构建

---
+++ test-data/src/main/java/com/stream/info/Event.java
@@ -0,0 +1,192 @@
+package com.stream.info;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Created by alpha on 2017/5/24.
+ */
+public class Event {
+
+    private String gid;
+
+    private String bizNo;
+
+    private String outBizNo;
+
+    private String userId;
+
+    private String event;
+
+    private String dataBizType;
+
+    private BigDecimal tradeAmont;
+
+    private String direction;
+
+    private String counterPartyUserId;
+
+    private String merchantUserId;
+
+    private String bankAccountNo;
+
+    private String bankAccountNoDigest;
+
+    private Date createTime;
+
+    private Date finshedTime;
+
+    private String dataDisposeSehedule;
+
+    private String dataOperateType;
+
+    public String getGid() {
+        return gid;
+    }
+
+    public void setGid(String gid) {
+        this.gid = gid;
+    }
+
+    public String getBizNo() {
+        return bizNo;
+    }
+
+    public void setBizNo(String bizNo) {
+        this.bizNo = bizNo;
+    }
+
+    public String getOutBizNo() {
+        return outBizNo;
+    }
+
+    public void setOutBizNo(String outBizNo) {
+        this.outBizNo = outBizNo;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getDataBizType() {
+        return dataBizType;
+    }
+
+    public void setDataBizType(String dataBizType) {
+        this.dataBizType = dataBizType;
+    }
+
+    public BigDecimal getTradeAmont() {
+        return tradeAmont;
+    }
+
+    public void setTradeAmont(BigDecimal tradeAmont) {
+        this.tradeAmont = tradeAmont;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getCounterPartyUserId() {
+        return counterPartyUserId;
+    }
+
+    public void setCounterPartyUserId(String counterPartyUserId) {
+        this.counterPartyUserId = counterPartyUserId;
+    }
+
+    public String getMerchantUserId() {
+        return merchantUserId;
+    }
+
+    public void setMerchantUserId(String merchantUserId) {
+        this.merchantUserId = merchantUserId;
+    }
+
+    public String getBankAccountNo() {
+        return bankAccountNo;
+    }
+
+    public void setBankAccountNo(String bankAccountNo) {
+        this.bankAccountNo = bankAccountNo;
+    }
+
+    public String getBankAccountNoDigest() {
+        return bankAccountNoDigest;
+    }
+
+    public void setBankAccountNoDigest(String bankAccountNoDigest) {
+        this.bankAccountNoDigest = bankAccountNoDigest;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getFinshedTime() {
+        return finshedTime;
+    }
+
+    public void setFinshedTime(Date finshedTime) {
+        this.finshedTime = finshedTime;
+    }
+
+    public String getDataDisposeSehedule() {
+        return dataDisposeSehedule;
+    }
+
+    public void setDataDisposeSehedule(String dataDisposeSehedule) {
+        this.dataDisposeSehedule = dataDisposeSehedule;
+    }
+
+    public String getDataOperateType() {
+        return dataOperateType;
+    }
+
+    public void setDataOperateType(String dataOperateType) {
+        this.dataOperateType = dataOperateType;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "gid='" + gid + '\'' +
+                ", bizNo='" + bizNo + '\'' +
+                ", outBizNo='" + outBizNo + '\'' +
+                ", userId='" + userId + '\'' +
+                ", event='" + event + '\'' +
+                ", dataBizType='" + dataBizType + '\'' +
+                ", tradeAmont=" + tradeAmont +
+                ", direction='" + direction + '\'' +
+                ", counterPartyUserId='" + counterPartyUserId + '\'' +
+                ", merchantUserId='" + merchantUserId + '\'' +
+                ", bankAccountNo='" + bankAccountNo + '\'' +
+                ", bankAccountNoDigest='" + bankAccountNoDigest + '\'' +
+                ", createTime=" + createTime +
+                ", finshedTime=" + finshedTime +
+                ", dataDisposeSehedule='" + dataDisposeSehedule + '\'' +
+                ", dataOperateType='" + dataOperateType + '\'' +
+                '}';
+    }
+}
