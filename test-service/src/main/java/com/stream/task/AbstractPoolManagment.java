fe917e0cdb2cdcecc58d8ec573d2ed9777d3cff1
zhangkirk@sohu.com <zhangkirk>
2017-05-25 11:18:50 +0800

    添加线程执行

---
+++ test-service/src/main/java/com/stream/task/AbstractPoolManagment.java
@@ -0,0 +1,19 @@
+package com.stream.task;
+
+import com.stream.pool.PoolManagement;
+import com.yjf.common.log.Logger;
+import com.yjf.common.log.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by Administrator on 2017/5/25.
+ */
+public abstract class AbstractPoolManagment<T> {
+
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private PoolManagement poolManagement;
+
+    public abstract void exec(T t);
+}
