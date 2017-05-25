f5bf82a7e96e8163d6324ffd5d306e8c25190d2a
zhangkirk@sohu.com <zhangkirk>
2017-05-25 11:19:17 +0800

    添加线程执行

---
+++ test-service/src/main/java/com/stream/task/TaskMonitor.java
@@ -0,0 +1,52 @@
+package com.stream.task;
+
+import com.stream.order.Event;
+import com.yjf.common.log.Logger;
+import com.yjf.common.log.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2017/5/25.
+ */
+@Component("taskMonitor")
+public class TaskMonitor  implements ApplicationListener<ContextRefreshedEvent> {
+
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @Autowired
+    private RandomDataTask randomDataTask;
+
+    private volatile boolean running = true;
+
+    private static final int threadMaxnum = 10;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        for (int i =1 ;i<=threadMaxnum; i++){
+            Thread thread = new Thread(new TaskWorker());
+            thread.setName("task-monitor" + i);
+            thread.setDaemon(true);
+            thread.start();
+        }
+    }
+
+    private class TaskWorker implements Runnable {
+
+        @Override
+        public void run() {
+            while (running) {
+                try {
+                    randomDataTask.exec(new Event());
+                } catch (Throwable throwable) {
+                    logger.error("出现意外错误……");
+                }
+            }
+        }
+
+    }
+
+}
