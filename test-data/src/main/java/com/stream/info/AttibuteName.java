cb74d8de1a37d14a9f7ba966cdac7ba0e395c142
huangkaitao <zijia@yiji.com>
2017-06-02 19:17:17 +0800

    请求包构建

--- test-data/src/main/java/com/stream/info/AttibuteName.java
+++ test-data/src/main/java/com/stream/info/AttibuteName.java
@@ -1,8 +1,15 @@
 package com.stream.info;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by alpha on 2017/6/2.
  */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface AttibuteName {
 
     String name();
